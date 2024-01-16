b1eb2063c81ddaf651bec568d2a44144a08ae4fa
MISEONGKIM <rlaaltjd46@naver.com>
2024-01-16 22:25:50 +0900

    feat : 장애물, 게임매니저, 플레이어 오브젝트 생성 및 장애물 충돌 시 gameover 함수 추가

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public bool isGameover { get; private set; } = false;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Debug.LogWarning("씬에 두 개 이상의 게임 매니저가 존재합니다.");
+            Destroy(gameObject);
+        }
+        
+    }
+
+   
+
+    public void OnPlayerDead()
+    {
+        isGameover = true;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static string  PLAYER_TAG = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player.PLAYER_TAG = gameObject.tag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
