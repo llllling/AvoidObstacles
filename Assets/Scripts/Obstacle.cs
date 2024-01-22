b330da9a13f551442858d7838394820bb509efb0
MISEONGKIM <rlaaltjd46@naver.com>
2024-01-23 00:12:00 +0900

    feat : 장애물 생성기 완료

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacle : MonoBehaviour
+public class Obstacle : Movement2D
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Start()
+    {
+        InitMovement(3f, Vector2.up);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag(Player.PLAYER_TAG))
         {
--- Assets/Scripts/ObstacleSpanwer.cs
+++ Assets/Scripts/ObstacleSpanwer.cs
@@ -1,35 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using UnityEngine.UIElements;
 
 public class ObstacleSpanwer : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    public int maxCount = 5;
+    public int count = 10;
+
+    public float placementMinTime = 0.25f;
+    public float placementMaxTime = 0.75f;
+    private float placementTime;
+    private float lastPlacementTime;
+
+    private float xPosMin;
+    private float xPosMax;
+    private readonly float yPos = -10f;
 
     private GameObject[] obstacles;
+    private int currentIndex = 0;
+
 
 
     void Start()
     {
 
-        //int count = Random.Range(minCount, minCount + 1);
-        //obstacles = new GameObject[count];
-        //for (int i = 0; i < count; i++)
-        //{
+        CreateObstacles();
+        InitXPosMinMax();
 
-        //    obstacles[i] = Instantiate(obstaclePrefab, GetRandomPosition(), Quaternion.identity);
-
-
-        //}
-
+        placementTime = 0f;
+        lastPlacementTime = 0f;
     }
     void Update()
     {
+        if (Time.time < lastPlacementTime + placementTime) return;
+
+        lastPlacementTime = Time.time;
+        
+        placementTime = Random.Range(placementMinTime, placementMaxTime);
+
+        obstacles[currentIndex++].transform.position = new Vector3(Random.Range(xPosMin, xPosMax), yPos);
+        
+        if (currentIndex == obstacles.Length)
+        {
+            currentIndex = 0;
+        }
 
     }
+    private void CreateObstacles()
+    {
+        Vector2 initPosition = new(0, -10f);
+        obstacles = new GameObject[count];
+        for (int i = 0; i < count; i++)
+        {
+            obstacles[i] = Instantiate(obstaclePrefab, initPosition, Quaternion.identity);
+        }
+    }
 
+    private void InitXPosMinMax()
+    {
+        Tilemap background = FindFirstObjectByType<Tilemap>();
+        float offsetX = background.size.x / 2f - 0.7f;
+
+        xPosMin = -offsetX;
+        xPosMax = offsetX;
+    }
   
 }
